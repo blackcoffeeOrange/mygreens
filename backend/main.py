1d321db3cdbdabbd991eb6711dbfc8b45788f4b3
Tanaka <blackcoffee.orange@gmail.com>
2023-05-31 08:38:14 +0900

    データベース接続ソース

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template #Flask
+import sqlite3 #データベース
+
+#データベースに接続
+conn = sqlite3.connect("Sample.sqlite3")
+#カーソル生成
+c = conn.cursor()
+#テーブルの存在を確認してみて無ければ作成（カラム　id、日時、温度）
+c.execute("CREATE TABLE IF NOT EXISTS temp_table (id INTEGER PRIMARY KEY AUTOINCREMENT, temp INTEGER, timestamp TEXT PRYMARYKEY UNIQUE)")
+#テーブル情報保存
+conn.commit()
+#データベース接続を閉じる
+conn.close()
+
 
 app = Flask(__name__, static_folder='../vue-project/dist/static', template_folder='../vue-project/dist')
 
@@ -6,6 +19,7 @@ app = Flask(__name__, static_folder='../vue-project/dist/static', template_folde
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
+    c.executemany("insert into temp_table (temp, timestamp) values (?, ?)", temp_time)
     return render_template('index.html')
 
 if __name__ == '__main__':
