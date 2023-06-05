6256c3fe62894da95563ef889f061950ec510ea2
Tanaka <blackcoffee.orange@gmail.com>
2023-06-05 19:32:50 +0900

    リアクティブ作成テスト

--- vue-project/src/components/List.vue
+++ vue-project/src/components/List.vue
@@ -1,48 +1,60 @@
-<script setup lang="ts">
+<script setup>
 import axios from 'axios'
-import { computed } from 'vue'
-import { ref } from 'vue';
-import { reactive } from 'vue'
+import { reactive,ref,computed } from 'vue'
 const state = reactive({ count: 0 })
 
-// const text = ref('');
-let listdata: any = ref();
+const title = ref('一覧');
+
+const plantList = ref([])
+const newPlant = ref('');
+const addPlant = () =>{
+    plantList.value.push(newPlant.value)
+}
+// let plantlist: any = ref();
 // let listdata: any = []
+// const number = computed(() => {
+//     return plantlist.length
+// })
+// axios.get('http://localhost:8000/green')
+//     .then(function (response) {
+//         // handle success(axiosの処理が成功した場合に処理させたいことを記述)
+//         plantlist = response.data
+//         for (let i = 0; i < plantlist.length; i++) {
 
-axios.get('http://localhost:8000/green')
-    .then(function (response) {
-        // handle success(axiosの処理が成功した場合に処理させたいことを記述)
-        listdata = response.data
-        for (let i = 0; i < listdata.length; i++) {
-
-            console.log(listdata[i]);
-        }
-        // console.log(listdata);
-    })
-    .catch(function (error) {
-        // handle error(axiosの処理にエラーが発生した場合に処理させたいことを記述)
-        console.log(error);
-    })
-    .finally(function () {
-        // always executed(axiosの処理結果によらずいつも実行させたい処理を記述)
-        console.log("終了");
-    });
+//             console.log(plantlist[i]);
+//         }
+//         // console.log(listdata);
+//     })
+//     .catch(function (error) {
+//         // handle error(axiosの処理にエラーが発生した場合に処理させたいことを記述)
+//         console.log(error);
+//     })
+//     .finally(function () {
+//         // always executed(axiosの処理結果によらずいつも実行させたい処理を記述)
+//         console.log("終了");
+//     });
 
 </script>
 
 <template>
+    <h3>{{ title }}({{ number }})</h3>
     <!-- API経由でデータを取得 -->
     <div class="list_component">
+        <input type="text" v-model="newPlant">
+        <button @click="addPlant()">
+                追加
+        </button>
         <ul>
-            <li v-for="item in listdata">
+            <li v-for="plant in plantlist">
                 <router-link to="/detail">
-                    {{ item.name }}
+                    {{ plant.id }}
                     <span class="icon chevron_right"></span>
                 </router-link>
             </li>
             <li>
-                <router-link to="/detail">新規作成</router-link>
+           
             </li>
         </ul>
+        
     </div>
 </template>
--- vue-project/src/views/Detail.vue
+++ vue-project/src/views/Detail.vue
@@ -1,18 +1,42 @@
 <script setup>
-import { reactive } from 'vue'
+import { ref, reactive } from 'vue'
 
+const name = ref('')
 const state = reactive({ count: 0 })
-
 function increment() {
   state.count++
+}
+
+const number = ref(0)
+const add = () =>{
+  number.value++
 }
 </script>
 
 <template>
     <div class="detail">
-            <h1>This is detail page</h1>
+            <h1>This is {{ name }} detail page</h1>
             <button @click="increment">
                 {{ state.count }}
             </button>
+            <dl>
+              <div>
+                <dt>
+                  name
+                </dt>
+                <dd>
+                  <input type="text" v-model="name">
+                </dd>
+              </div>
+              <div>
+                <dt>
+                  number
+                </dt>
+                <dd>
+                  <input type="text" v-model="number">
+                  <button @click="add()">+1</button>
+                </dd>
+              </div>
+            </dl>
         </div>
 </template>
