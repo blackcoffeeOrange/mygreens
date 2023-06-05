29bfc431ce9d7138feb388e472b49a0a9c150eff
Tanaka <blackcoffee.orange@gmail.com>
2023-06-05 09:22:24 +0900

    データベース読み込み一覧

--- vue-project/src/components/List.vue
+++ vue-project/src/components/List.vue
@@ -1,36 +1,42 @@
-<script setup>
+<script setup lang="ts">
 import axios from 'axios'
-import {computed} from 'vue'
-
+import { computed } from 'vue'
+import { ref } from 'vue';
 import { reactive } from 'vue'
 const state = reactive({ count: 0 })
 
-// let listdata =[]
+// const text = ref('');
+let listdata: any = ref();
+// let listdata: any = []
 
-// axios.get('http://localhost:8000/green')
-//     .then(function (response) {
-//         // handle success(axiosの処理が成功した場合に処理させたいことを記述)
-//        this.listdata = response.data
-//         console.log(listdata);
-//     })
-//     .catch(function (error) {
-//         // handle error(axiosの処理にエラーが発生した場合に処理させたいことを記述)
-//         // console.log(error);
-//     })
-//     .finally(function () {
-//         // always executed(axiosの処理結果によらずいつも実行させたい処理を記述)
-//         console.log("終了");
-//     });
-    
+axios.get('http://localhost:8000/green')
+    .then(function (response) {
+        // handle success(axiosの処理が成功した場合に処理させたいことを記述)
+        listdata = response.data
+        for (let i = 0; i < listdata.length; i++) {
+
+            console.log(listdata[i]);
+        }
+        // console.log(listdata);
+    })
+    .catch(function (error) {
+        // handle error(axiosの処理にエラーが発生した場合に処理させたいことを記述)
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed(axiosの処理結果によらずいつも実行させたい処理を記述)
+        console.log("終了");
+    });
+
 </script>
 
 <template>
     <!-- API経由でデータを取得 -->
     <div class="list_component">
         <ul>
-          <li v-for="item in listdata">
+            <li v-for="item in listdata">
                 <router-link to="/detail">
-                    {{ item }}
+                    {{ item.name }}
                     <span class="icon chevron_right"></span>
                 </router-link>
             </li>
