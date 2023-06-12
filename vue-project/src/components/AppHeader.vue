725c17deb81ab000bddc14bfb15666e83534eefa
Tanaka <blackcoffee.orange@gmail.com>
2023-06-12 17:56:20 +0900

    no message

---
+++ vue-project/src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// colorというpropsを用意してね
+// 親からこのコンポーネントに値を渡せる
+    defineProps(['color'])
+</script>
+<template>
+    <header>
+        <h1 :style="{'color': color }">
+            <slot>No title</slot>
+            Todoツール
+        </h1>
+    </header>
+</template>
---
+++ vue-project/src/store/store.js
@@ -0,0 +1,29 @@
+import {createStore} from 'vuex'
+
+
+export default createStore({
+    state:{
+
+    },
+    getters:{
+
+    },
+    mutations:{
+        save(state){
+
+        },
+        delete(state, id){
+    
+        },
+
+    },
+    actions:{
+
+    },
+    modules:{
+        // products
+    },
+    pludgins:{
+
+    }
+})
